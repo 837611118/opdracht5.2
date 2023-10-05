8345fc7ef446413bdf9909a7d210a9be04234675
837611118 <144031540+837611118@users.noreply.github.com>
2023-10-05 10:32:02 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+function KleurEnTekstAanpassen()
+{
+    document.getElementById("kop1").style.color="blueviolet";
+    document.getElementById("demo").innerHTML="I have changed!";
+}
+
+function Reset()
+{
+    document.getElementById("kop1").style.color="black";
+    document.getElementById("kop1").innerHTML="I have changed!";   
+}
